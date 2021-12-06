70efb7e9559ea38af5593846131446f648db9e0e
RawnakAra <rawnak96@gmail.com>
2021-12-06 23:24:12 +0200

    search

--- webData/controller/recipes.controller.js
+++ webData/controller/recipes.controller.js
@@ -1,70 +1,70 @@
 const recipeModel = require('../models/recipes.module').recipeModel
 
-const getAllRecipe =async (req, res) => {
- try{
-  recipeModel.find({}, (err, data) => {
-    if (data)
-      return res.status(200).json(data)
-    return res.status(400).json(err)
-  })
-}catch(e){
-  res.status(503).send(e.massege)
-}
-}
-
-const searchForRecipeByName =async (req, res) => {
-  const { recipeNameToSearch } = req.body
-  try{ 
+const getAllRecipe = async (req, res) => {
+  try {
     recipeModel.find({}, (err, data) => {
-    if (err)
-    return res.status(400).json(err)
-    if(data){
-     const searchedData = (data.filter(r=> r.recipeName.toString().toLowerCase().includes(recipeNameToSearch.trim().toLowerCase())))
-     return res.status(200).send(searchedData)
-    }
-  })
-}catch(e){
-  res.status(503).send(e.massege)
-}
+      if (data)
+        return res.status(200).json(data)
+      return res.status(400).json(err)
+    })
+  } catch (e) {
+    res.status(503).send(e.massege)
+  }
 }
 
-const searchByIngredients =async (req, res) => {
+const searchForRecipeByName = async (req, res) => {
+  const { recipeNameToSearch } = req.body
+  try {
+    recipeModel.find({}, (err, data) => {
+      if (err)
+        return res.status(400).json(err)
+      if (data) {
+        const searchedData = (data.filter(r => r.recipeName.toString().toLowerCase().includes(recipeNameToSearch.toLowerCase())))
+        return res.status(200).send(searchedData)
+      }
+    })
+  } catch (e) {
+    res.status(503).send(e.massege)
+  }
+}
+
+const searchByIngredients = async (req, res) => {
   const { recipeIngredients } = req.body
- try{
-  recipeModel.find({}, (err, data) => {
-    //console.log(data)
-    if (data){
-     const searchData = data.filter(recipe=>{
-        let isTrue = 0
-       recipeIngredients.map(ingredient =>{
-        // console.log(isTrue)
-         if(recipe.ingredients.toString().toLowerCase().includes(ingredient.toString().toLowerCase())) 
-          return isTrue += 1
+  try {
+    recipeModel.find({}, (err, data) => {
+      //console.log(data)
+      if (data) {
+        const searchData = data.filter(recipe => {
+          let isTrue = 0
+          recipeIngredients.map(ingredient => {
+            // console.log(isTrue)
+            if (recipe.ingredients.toString().toLowerCase().includes(ingredient.toString().toLowerCase()))
+              return isTrue += 1
+          })
+          if (isTrue === recipeIngredients.length) {
+            return recipe
+          }
         })
-        if(isTrue === recipeIngredients.length) {
-          return recipe
-       }
-    })  
-   // console.log(searchData)
-    return res.status(200).send(searchData)
-    }
-    return res.status(400).json(err)
-  })
-}catch(e){
-  res.status(503).send(e.massege)
-}
+        // console.log(searchData)
+        return res.status(200).send(searchData)
+      }
+      return res.status(400).json(err)
+    })
+  } catch (e) {
+    res.status(503).send(e.massege)
+  }
 }
 
-const postANewRecipe =async (req, res) => {
+const postANewRecipe = async (req, res) => {
   let newLink = new recipes(req.body)
- try{ 
-  recipeModel.save((err, data) => {
-    if (err) return res.status(404).send(err)
-    return res.status(200).send(data)
-  })
-}catch(e){
-  res.status(503).send(e.massege)
-}
+  try {
+    recipeModel.save((err, data) => {
+      if (err) return res.status(404).send(err)
+      return res.status(200).send(data)
+    })
+  } catch (e) {
+    res.status(503).send(e.massege)
+  }
 }
 
 
