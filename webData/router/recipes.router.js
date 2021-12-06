781a0caefa036e3d6fd8943d11769e6b383d86c3
RawnakAra <rawnak96@gmail.com>
2021-12-06 14:44:30 +0200

    search

--- webData/router/recipes.router.js
+++ webData/router/recipes.router.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const recipesCntroller = require('../controller/recipes.controller')
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
    await recipesCntroller.getAllRecipe(req, res)
-}).get('/searchbyname',(req,res)=>{
+}).get('/searchbyname',async (req,res)=>{
     await recipesCntroller.searchForRecipeByName(req,res)
-}).get('/searchbyingredients' , (req,res)=>{
+}).get('/searchbyingredients' ,async (req,res)=>{
     await recipesCntroller.searchByIngredients(req,res)
-}).post('/', (req, res) => {
+}).post('/', async(req, res) => {
    await recipesCntroller.postANewRecipe(req, res)
 })
 
